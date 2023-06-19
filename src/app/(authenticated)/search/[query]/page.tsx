36b86a5efde2a3e45fd4a8a8367e3e96e9f8b6ee
Ankit Kumar <myselfankit51@gmail.com>
2023-06-20 03:23:40 +0530

    changed search page layout for tracks in all tab

--- src/app/(authenticated)/search/[query]/page.tsx
+++ src/app/(authenticated)/search/[query]/page.tsx
@@ -2,15 +2,10 @@ import AlbumCards from "@/components/AlbumCards";
 import ArtistCards from "@/components/ArtistCards";
 import PlaylistCards from "@/components/PlaylistCards";
 import SearchFilters from "@/components/SearchFilters";
-import SearchResultHeader from "@/components/SearchResultHeader";
 import TracksTable from "@/components/TracksTable";
 import { getSearchItems } from "@/lib/actions";
-import { Artist } from "@/types/types";
-import { fmtMSS } from "@/utils/clientUtils";
 import { getAuthSession } from "@/utils/serverUtils";
 import { Metadata } from "next";
-import Image from "next/image";
-import Link from "next/link";
 import { redirect } from "next/navigation";
 
 interface Props {
@@ -49,44 +44,6 @@ export default async function SearchResults({ params }: Props) {
           showCover
           showSubtitle
         />
-
-        {/* {searchResults.tracks.items.map((track: any) => (
-          <div
-            className="grid items-center grid-cols-12 col-span-12 my-3"
-            key={track.id}
-          >
-            <div className="flex items-center w-full col-span-11">
-              <Image
-                src={track.album.images[0].url}
-                alt={track.name}
-                width={40}
-                height={40}
-                className="object-contain w-10 h-10 rounded"
-              />
-
-              <div className="flex flex-col w-10/12 ml-4 text-sm font-medium truncate">
-                <h4>{track.name}</h4>
-
-                <div className="flex flex-wrap items-center w-10/12 gap-1 text-sm text-gray">
-                  {track.artists.map((artist: Artist, index: number) => (
-                    <>
-                      {index !== 0 && ", "}
-                      <Link key={artist.id} href={`/artists/${artist.id}`}>
-                        <span className="truncate hover:text-white hover:underline">
-                          {artist.name}
-                        </span>
-                      </Link>
-                    </>
-                  ))}
-                </div>
-              </div>
-            </div>
-
-            <span className="flex justify-end w-full col-span-1 text-sm text-gray">
-              {fmtMSS(track.duration_ms)}
-            </span>
-          </div>
-        ))} */}
       </div>
 
       {searchResults.artists.items.length > 0 && (
