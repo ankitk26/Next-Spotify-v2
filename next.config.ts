c4c9e484cb5109230fec0c8a91faac0fe5bb6cbc
ankitk26 <myselfankit51@gmail.com>
2025-08-12 00:15:35 +0530

    fix typo

--- next.config.ts
+++ next.config.ts
@@ -17,7 +17,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "*.cloudindary.com",
+        hostname: "res.cloudinary.com",
         port: "",
         pathname: "/**",
       },
